0acbcb29ce248872f5bcf23662b53edfe18fdc60
molinapetitts <molinapetitts@T29681.niaccit.niacc.edu>
2020-10-15 09:55:29 -0500

    Added a some lines of code to ask user for 5 numbers

--- public_html/LoopsPractice2.js
+++ public_html/LoopsPractice2.js
@@ -5,13 +5,12 @@
  */
 
 document.write("This program asks the user for 5 numbers and prints their total.");
-let counter = 1;
-let sum = 0;
+let total = 0;
 let userNumber = 0;
-while (counter <= 5) {
-    userNumber = Number(prompt("Enter number " + counter + ": "));
-    sum = sum + userNumber;
+let counter = 1;
+while (counter <= 5);{
+    userNumber = prompt("Enter an integer:");
+    userNumber = Number(userNumber);
+    total = total + userNumber;
     counter = counter + 1;
 }
-document.write("<br>The sum is " + sum);
-
